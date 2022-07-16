1e6decc036e77391ddfef50271304f38813cfcd7
absahil <08absahil@gmail.com>
2022-07-16 08:35:21 +0530

    removed version key

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     _id : String
 }, {
-    versionKey : false;
+    versionKey : false
 })
 const User = mongoose.model('User', userSchema);
 module.exports = User;
